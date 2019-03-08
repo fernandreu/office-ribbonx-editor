f3a2497d7a563a59bbbe26df95d340e0be5e9e2b
Fernando <2480661+fernandreu@users.noreply.github.com>
2019-03-08 22:25:02 +0000

    Fix powershell script

--- PreBuild.ps1
+++ PreBuild.ps1
@@ -1,5 +1,5 @@
-Write-Host "Build number is: $args[0]"
+Write-Host "Build number is: $($args[0])"
 $parts = $args[0].Split(".")
-Write-Host "Split into MSBuildNumber=$parts[0]; MSRevision=$parts[1]"
-Write-Host "##vso[task.setvariable variable=MSBuildNumber;]$parts[0]"
-Write-Host "##vso[task.setvariable variable=MSBuildNumber;]$parts[1]"
+Write-Host "Split into MSBuildNumber=$($parts[0]); MSRevision=$($parts[1])"
+Write-Host "##vso[task.setvariable variable=MSBuildNumber;]$($parts[0])"
+Write-Host "##vso[task.setvariable variable=MSBuildNumber;]$($parts[1])"
