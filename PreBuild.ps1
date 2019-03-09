0c359ae02995aeb3c737b482d2a4d7ec528efd26
Fernando <2480661+fernandreu@users.noreply.github.com>
2019-03-09 04:00:48 +0000

    Scrap build extensions and do everything through powershell

--- PreBuild.ps1
+++ PreBuild.ps1
@@ -3,3 +3,9 @@ $parts = $args[0].Split(".")
 Write-Host "Split into MSBuildNumber=$($parts[0]); MSRevision=$($parts[1])"
 Write-Host "##vso[task.setvariable variable=MSBuildNumber;]$($parts[0])"
 Write-Host "##vso[task.setvariable variable=MSRevision;]$($parts[1])"
+Get-ChildItem "SharedAssemblyInfo.cs" |
+ForEach-Object {
+    $c = ($_ | Get-Content)
+    $c = $c -replace '(AssemblyVersion\(\"\d?.\d?)\.(\*)(\"\))', "`$1.$($parts[0]).$($parts[1])`$3"
+    [IO.File]::WriteAllText($_.FullName, ($c -join "`r`n"))
+}
