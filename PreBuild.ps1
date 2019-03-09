5a81cee5f917815336e1c6efd17345b9cbc7bd00
Fernando <2480661+fernandreu@users.noreply.github.com>
2019-03-09 04:24:27 +0000

    Fix build numbering system

--- PreBuild.ps1
+++ PreBuild.ps1
@@ -1,13 +1,17 @@
 Write-Host "Build number is: $($args[0])"
+# Assembly version has length limitations, so we need to recombine this differently
 $parts = $args[0].Split(".")
+$tmp = $parts[0]
+$parts[0] = $parts[0].Substring(2, 4)
+$parts[1] = "$($tmp.Substring(6,2))$($parts[1].PadLeft(3, '0'))"
 Write-Host "Split into MSBuildNumber=$($parts[0]); MSRevision=$($parts[1])"
 Write-Host "##vso[task.setvariable variable=MSBuildNumber;]$($parts[0])"
 Write-Host "##vso[task.setvariable variable=MSRevision;]$($parts[1])"
 Get-ChildItem "SharedAssemblyInfo.cs" |
 ForEach-Object {
-    $c = ($_ | Get-Content)
+    $c = ($_ | Get-Content -encoding Unicode)
     $c = $c -replace '(AssemblyVersion\(\"\d?.\d?)\.(\*)(\"\))', "`$1.$($parts[0]).$($parts[1])`$3"
     $joined = $c -join "`r`n"
-    Write-Host "Resulting assembly:`n$joined"
+    # Write-Host "Resulting assembly:`n$joined"
     [IO.File]::WriteAllText($_.FullName, $joined)
 }
