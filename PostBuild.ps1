4e07b5fc4633bc80139eaa6d9387fc13b1f5b755
Fernando <2480661+fernandreu@users.noreply.github.com>
2019-03-13 23:33:13 +0000

    Test post build step again

--- PostBuild.ps1
+++ PostBuild.ps1
@@ -3,7 +3,8 @@ cd tmp
 git clone --single-branch --branch info https://github.com/fernandreu/wpf-custom-ui-editor.git -q
 cd wpf-custom-ui-editor
 $version = [string]$args[0]
+$pat = [string]$args[1]
 Write-Host "Version passed is: $version"
 $version | Out-File -Encoding UTF8 -FilePath .\RELEASE-VERSION
 git commit -a -m "Update release version to $version" -q
-git push https://github.com/fernandreu/wpf-custom-ui-editor.git info -q
+git push https://$($pat)@github.com/fernandreu/wpf-custom-ui-editor.git info -q
