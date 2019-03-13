499a61fbe02a5505c7fe6ae88eb93416e758a96a
Fernando <2480661+fernandreu@users.noreply.github.com>
2019-03-13 22:35:04 +0000

    Use revision only for automatic versioning

--- PreBuild.ps1
+++ PreBuild.ps1
@@ -1,17 +1,22 @@
-Write-Host "Build number is: $($args[0])"
-# Assembly version has length limitations, so we need to recombine this differently
-$parts = $args[0].Split(".")
-$tmp = $parts[0]
-$parts[0] = $parts[0].Substring(2, 4)
-$parts[1] = "$($tmp.Substring(6,2))$($parts[1].PadLeft(3, '0'))"
-Write-Host "Split into MSBuildNumber=$($parts[0]); MSRevision=$($parts[1])"
-Write-Host "##vso[task.setvariable variable=MSBuildNumber;]$($parts[0])"
-Write-Host "##vso[task.setvariable variable=MSRevision;]$($parts[1])"
+$buildId = $args[0]
+Write-Host "Build ID is: $buildId"
+$version = [string]$args[1]
+if ($version.StartsWith("v")) {
+    $parts = $version.Split(".")
+    while ($parts.Count -lt 3) {
+        $parts += "0"
+    }
+    $version = $parts -join "."
+    Write-Host "Resulting version: $version"
+} else {
+    Write-Host "No version detected"
+}
+Write-Host "##vso[task.setvariable variable=ThreeDigitVersion;]$version"
 Get-ChildItem "SharedAssemblyInfo.cs" |
 ForEach-Object {
-    $c = ($_ | Get-Content -encoding Unicode)
-    $c = $c -replace '(AssemblyVersion\(\"\d?.\d?)\.(\*)(\"\))', "`$1.$($parts[0]).$($parts[1])`$3"
+    $c = ($_ | Get-Content -encoding UTF8)
+    $c = $c -replace '(AssemblyVersion\(\"\d+.\d+.\d+)\.(\*)(\"\))', "`$1.$buildId`$3"
     $joined = $c -join "`r`n"
-    # Write-Host "Resulting assembly:`n$joined"
+    Write-Host "Resulting assembly:`n$joined"
     [IO.File]::WriteAllText($_.FullName, $joined)
 }
