0622761b75fb342f561e94150e954fd301e053c7
Fernando <2480661+fernandreu@users.noreply.github.com>
2019-03-13 23:51:11 +0000

    Move GitHub details to pipeline variables

--- PostBuild.ps1
+++ PostBuild.ps1
@@ -4,9 +4,11 @@ git clone --single-branch --branch info https://github.com/fernandreu/wpf-custom
 cd wpf-custom-ui-editor
 $version = [string]$args[0]
 $pat = [string]$args[1]
+$name = [string]$args[2]
+$email = [string]$args[3]
 Write-Host "Version passed is: $version"
 $version | Out-File -Encoding UTF8 -FilePath .\RELEASE-VERSION
-git config --global user.email "2480661+fernandreu@users.noreply.github.com"
-git config --global user.name "Fernando Andreu"
+git config --global user.email "$email"
+git config --global user.name "$name"
 git commit -a -m "Update release version to $version" -q
 git push https://$($pat)@github.com/fernandreu/wpf-custom-ui-editor.git info -q
