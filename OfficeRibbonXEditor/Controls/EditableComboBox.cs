6a65b898890fbc337914bf55e44146b198d23425
Fernando <2480661+fernandreu@users.noreply.github.com>
2019-07-07 14:15:42 +0100

    Fix regression bug with selection of first find character

--- OfficeRibbonXEditor/Controls/EditableComboBox.cs
+++ OfficeRibbonXEditor/Controls/EditableComboBox.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace OfficeRibbonXEditor.Controls
 {
@@ -18,6 +20,41 @@ namespace OfficeRibbonXEditor.Controls
         {
             base.OnApplyTemplate();
             this.TextBox.TextChanged += this.OnTextChanged;
+            this.AddHandler(PreviewMouseLeftButtonDownEvent, 
+                new MouseButtonEventHandler(SelectivelyIgnoreMouseButton), true);
+            this.AddHandler(GotKeyboardFocusEvent, 
+                new RoutedEventHandler(SelectAllText), true);
+            this.AddHandler(MouseDoubleClickEvent, 
+                new RoutedEventHandler(SelectAllText), true);
+        }
+
+        private static void SelectivelyIgnoreMouseButton(object sender, 
+            MouseButtonEventArgs e)
+        {
+            // Find the TextBox
+            DependencyObject parent = e.OriginalSource as UIElement;
+            while (parent != null && !(parent is TextBox))
+                parent = VisualTreeHelper.GetParent(parent);
+
+            if (parent != null)
+            {
+                var textBox = (TextBox)parent;
+                if (!textBox.IsKeyboardFocusWithin)
+                {
+                    // If the text box is not yet focussed, give it the focus and
+                    // stop further processing of this click event.
+                    textBox.Focus();
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private static void SelectAllText(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource is TextBox textBox)
+            {
+                textBox.SelectAll();
+            }
         }
 
         protected override void OnGotFocus(RoutedEventArgs e)
@@ -25,7 +62,6 @@ namespace OfficeRibbonXEditor.Controls
             base.OnGotFocus(e);
 
             this.TextBox?.Focus();
-            this.TextBox?.SelectAll();
         }
 
         private void OnTextChanged(object sender, TextChangedEventArgs e)
@@ -33,7 +69,6 @@ namespace OfficeRibbonXEditor.Controls
             var txt = (TextBox) sender;
             txt.TextChanged -= this.OnTextChanged;
             txt.Focus();
-            txt.SelectAll();
         }
     }
 }
