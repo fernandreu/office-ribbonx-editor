92241ee64beee9def1d447df04402b89f74c0e66
Fernando Andreu <2480661+fernandreu@users.noreply.github.com>
2020-02-12 23:58:25 +0000

    Pass osslsigncode args explicitly

--- build/sign/sign-linux.ps1
+++ build/sign/sign-linux.ps1
@@ -13,14 +13,13 @@ function Set-SignatureLocally {
 
     Write-Host "Signing file"
     $resultingName = "$($FileInfo.BaseName)-Signed$($FileInfo.Extension)"
-    $commonArgs = "-pkcs11engine /usr/lib/x86_64-linux-gnu/engines-1.1/pkcs11.so -pkcs11module /opt/proCertumCardManager/sc30pkcs11-2.0.0.39.r2-MS.so -certs ~/codesign.spc -t http://time.certum.pl -pass $Pin"
     if ($FileInfo.Extension -eq ".exe") {
         # Perform a dual signature
-        . osslsigncode $commonArgs -h sha1 -in "/tmp/$($FileInfo.Name)" -out "/tmp/$resultingName.tmp"
-        . osslsigncode $commonArgs -nest -h sha2 -in "/tmp/$resultingName.tmp" -out "/tmp/$resultingName"
+        . osslsigncode -pkcs11engine /usr/lib/x86_64-linux-gnu/engines-1.1/pkcs11.so -pkcs11module /opt/proCertumCardManager/sc30pkcs11-2.0.0.39.r2-MS.so -certs ~/codesign.spc -t http://time.certum.pl -pass $Pin -h sha1 -in "/tmp/$($FileInfo.Name)" -out "/tmp/$resultingName.tmp"
+        . osslsigncode -pkcs11engine /usr/lib/x86_64-linux-gnu/engines-1.1/pkcs11.so -pkcs11module /opt/proCertumCardManager/sc30pkcs11-2.0.0.39.r2-MS.so -certs ~/codesign.spc -t http://time.certum.pl -pass $Pin -nest -h sha2 -in "/tmp/$resultingName.tmp" -out "/tmp/$resultingName"
     } else {
         # Use only SHA256 signature (as it might not be possible to dual-sign the file)
-        . osslsigncode $commonArgs -h sha2 -in "/tmp/$($FileInfo.Name)" -out "/tmp/$resultingName"
+        . osslsigncode -pkcs11engine /usr/lib/x86_64-linux-gnu/engines-1.1/pkcs11.so -pkcs11module /opt/proCertumCardManager/sc30pkcs11-2.0.0.39.r2-MS.so -certs ~/codesign.spc -t http://time.certum.pl -pass $Pin -h sha2 -in "/tmp/$($FileInfo.Name)" -out "/tmp/$resultingName"
     }
     if ($LASTEXITCODE -ne 0) {
         return $false
