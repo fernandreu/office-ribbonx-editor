d177758da986abe5b8309d0c3c51910c6064d7ff
Fernando Andreu <2480661+fernandreu@users.noreply.github.com>
2020-11-18 00:02:40 +0000

    Add new certificate signing

--- build/sign/sign-windows.ps1
+++ build/sign/sign-windows.ps1
@@ -1,32 +1,23 @@
-[CmdletBinding(PositionalBinding=$false)]
-param (
+$_SIGN_TOOL = 'C:\Program Files (x86)\Windows Kits\10\App Certification Kit\signtool.exe'
 
-    [string]$signTool = 'C:\Program Files (x86)\Windows Kits\10\App Certification Kit\signtool.exe',
+$_CERT_NAME = 'Open Source Developer, Fernando Andreu'
 
-    [string]$certName = 'Open Source Developer, Fernando Andreu',
+function Set-Signatures {
+    param(
+        [string] $Sources
+    )
 
-    [Parameter(Mandatory=$true, ValueFromRemainingArguments=$true)]
-    [string[]]$files
-)
+    $fileList = @(Get-ChildItem -Path $Source -Include *.exe,*.dll | ForEach-Object { "`"$($_.FullName)`"" }) -join ' '
+    $arguments = "sign /n `"$_CERT_NAME`" /t http://time.certum.pl/ /fd sha1 /v $fileList"
+    Start-Process -FilePath "$_SIGN_TOOL" -NoNewWindow -Wait -ArgumentList $arguments
+    $arguments = "sign /n `"$_CERT_NAME`" /tr http://time.certum.pl/ /fd sha256 /as /v $fileList"
+    Start-Process -FilePath "$_SIGN_TOOL" -NoNewWindow -Wait -ArgumentList $arguments
 
-# sha256 signing
-
-# Dual signing only for supported files
-$filtered = @($files.where{ $_ -match '.exe' })
-if ($filtered.Count -gt 0) {
-    $fileList = @($filtered | ForEach-Object { "`"$_`"" }) -join ' '
-    $arguments = "sign /n `"$certName`" /t http://time.certum.pl/ /fd sha1 /v $fileList"
-    Start-Process -FilePath "$signTool" -NoNewWindow -Wait -ArgumentList $arguments
-
-    $fileList = @($filtered | ForEach-Object { "`"$_`"" }) -join ' '
-    $arguments = "sign /n `"$certName`" /tr http://time.certum.pl/ /fd sha256 /as /v $fileList"
-    Start-Process -FilePath "$signTool" -NoNewWindow -Wait -ArgumentList $arguments
+    $fileList = @(Get-ChildItem -Path $Source -Include *.msi | ForEach-Object { "`"$($_.FullName)`"" }) -join ' '
+    $arguments = "sign /n `"$_CERT_NAME`" /tr http://time.certum.pl/ /fd sha256 /as /v $fileList"
+    Start-Process -FilePath "$_SIGN_TOOL" -NoNewWindow -Wait -ArgumentList $arguments
 }
 
-# sha256 signing for everything else
-$filtered = @($files.where{ -not ($_ -match '.exe') })
-if ($filtered.Count -gt 0) {
-    $fileList = @($filtered | ForEach-Object { "`"$_`"" }) -join ' '
-    $arguments = "sign /n `"$certName`" /tr http://time.certum.pl/ /fd sha256 /v $fileList"
-    Start-Process -FilePath "$signTool" -NoNewWindow -Wait -ArgumentList $arguments
+If ($MyInvocation.InvocationName -ne '.') {
+    Set-Signatures $args[0]
 }
