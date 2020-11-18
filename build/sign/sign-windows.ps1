c231f266a9219c2148eff5352b47f34b7e516c38
Fernando Andreu <2480661+fernandreu@users.noreply.github.com>
2020-11-18 01:41:49 +0000

    Fix codesign script

--- build/sign/sign-windows.ps1
+++ build/sign/sign-windows.ps1
@@ -2,20 +2,52 @@ $_SIGN_TOOL = 'C:\Program Files (x86)\Windows Kits\10\App Certification Kit\sign
 
 $_CERT_NAME = 'Open Source Developer, Fernando Andreu'
 
-function Set-Signatures {
+function Get-IsSuitable {
     param(
-        [string] $Sources
+        [Object] $File,
+        [string[]] $Filters
     )
 
-    $fileList = @(Get-ChildItem -Path $Source -Include *.exe,*.dll | ForEach-Object { "`"$($_.FullName)`"" }) -join ' '
-    $arguments = "sign /n `"$_CERT_NAME`" /t http://time.certum.pl/ /fd sha1 /v $fileList"
-    Start-Process -FilePath "$_SIGN_TOOL" -NoNewWindow -Wait -ArgumentList $arguments
-    $arguments = "sign /n `"$_CERT_NAME`" /tr http://time.certum.pl/ /fd sha256 /as /v $fileList"
-    Start-Process -FilePath "$_SIGN_TOOL" -NoNewWindow -Wait -ArgumentList $arguments
+    foreach ($filter in $Filters) {
+        if ($File.Name -like $filter) {
+            return $true
+        }
+    }
 
-    $fileList = @(Get-ChildItem -Path $Source -Include *.msi | ForEach-Object { "`"$($_.FullName)`"" }) -join ' '
-    $arguments = "sign /n `"$_CERT_NAME`" /tr http://time.certum.pl/ /fd sha256 /as /v $fileList"
-    Start-Process -FilePath "$_SIGN_TOOL" -NoNewWindow -Wait -ArgumentList $arguments
+    return $false
+}
+
+function Get-Files {
+    param(
+        [string] $Source,
+        [string[]] $Filters
+    )
+
+    $files = [System.Collections.Generic.List[string]]@()
+    foreach ($file in (Get-ChildItem -Path $Source -File -Recurse)) {
+        if (Get-IsSuitable -File $file -Filters $Filters) {
+            $files.Add("`"$file`"") | Out-Null
+        }
+    }
+
+    return $files
+}
+
+function Set-Signatures {
+    param(
+        [string] $Source
+    )
+
+    # Only performing sha256 signatures for now. Dlls might need to be left untouched to speed things up
+    $files = Get-Files -Source $Source -Filters *.msi,*.exe,*.dll
+    if ($files.Count -ne 0) {
+        Write-Host "Found $($files.Count) files to single-sign"
+        $fileList = $files -join ' '
+        $arguments = "sign /n `"$_CERT_NAME`" /tr http://time.certum.pl /fd sha256 /a $fileList"
+        Start-Process -FilePath "$_SIGN_TOOL" -NoNewWindow -Wait -ArgumentList $arguments
+    } else {
+        Write-Host 'Found no files to single-sign'
+    }
 }
 
 If ($MyInvocation.InvocationName -ne '.') {
