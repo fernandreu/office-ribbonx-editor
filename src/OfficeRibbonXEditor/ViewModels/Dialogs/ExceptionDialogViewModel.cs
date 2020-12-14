6c1551160a573fc61949345c7b94c985320e5b3c
Fernando Andreu <2480661+fernandreu@users.noreply.github.com>
2020-12-14 18:42:13 +0000

    Remove double encoding when sending bug report (#134)

--- src/OfficeRibbonXEditor/ViewModels/Dialogs/ExceptionDialogViewModel.cs
+++ src/OfficeRibbonXEditor/ViewModels/Dialogs/ExceptionDialogViewModel.cs
@@ -46,18 +46,18 @@ namespace OfficeRibbonXEditor.ViewModels.Dialogs
 
         private void ExecuteSubmitBugCommand()
         {
-            var title = Uri.EscapeDataString($"{this.exception?.GetType().Name}: {this.Exception?.Message}");
-            var body = Uri.EscapeDataString(
-                "**Describe the bug**\n\n" +
-                $"```\n{this.exception}\n```\n\n" +
-                "**To Reproduce**\n" +
-                "Please describe the steps that lead to the unhandled exception here, including example files if relevant.\n\n" +
-                "**Screenshots**\n" +
-                "If applicable, add screenshots to help explain how the unhandled exception occurred.\n\n" +
-                "**Additional context**\n\n" +
-                $"- Version: {this.info.AssemblyVersion}\n" +
-                $"- Runtime: {this.info.RuntimeVersion}\n" +
-                $"- Operating System: {this.info.OperatingSystemVersion}\n");
+            var title =$"{this.exception?.GetType().Name}: {this.Exception?.Message}";
+            var body = 
+               "**Describe the bug**\n\n" +
+               $"```\n{this.exception}\n```\n\n" +
+               "**To Reproduce**\n" +
+               "Please describe the steps that lead to the unhandled exception here, including example files if relevant.\n\n" +
+               "**Screenshots**\n" +
+               "If applicable, add screenshots to help explain how the unhandled exception occurred.\n\n" +
+               "**Additional context**\n\n" +
+               $"- Version: {this.info.AssemblyVersion}\n" +
+               $"- Runtime: {this.info.RuntimeVersion}\n" +
+               $"- Operating System: {this.info.OperatingSystemVersion}\n";
             this.urlHelper.OpenBug(title, body);
         }
     }
