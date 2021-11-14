b248cd27b1b0371b513b3d28a14278dc845e75e1
Fernando Andreu <2480661+fernandreu@users.noreply.github.com>
2021-11-14 00:35:48 +0000

    Fix the PresentationFramework.dll copy for .NET

--- build/Scripts/CopyLibraries.ps1
+++ build/Scripts/CopyLibraries.ps1
@@ -1,8 +1,43 @@
 ﻿$buildConfiguration = $args[0]
 $targetFramework = $args[1]
-# TODO: If possible, the following should also be passed via a build variable
-$winPath = $env:WINDIR
-$dllPath = "$winPath/Microsoft.NET/Framework64/v4.0.30319/WPF/PresentationFramework.dll"
+
+$runtimes = @(dotnet --list-runtimes)
+
+# The command above should produce an output in the following format:
+# Microsoft.AspNetCore.All 2.1.26 [C:\Program Files\dotnet\shared\Microsoft.AspNetCore.All]
+# ...
+# Microsoft.WindowsDesktop.App 5.0.4 [C:\Program Files\dotnet\shared\Microsoft.WindowsDesktop.App]
+# Microsoft.WindowsDesktop.App 6.0.0 [C:\Program Files\dotnet\shared\Microsoft.WindowsDesktop.App]
+
+# We will search for the highest version of Microsoft.WindowsDesktop.App
+$runtimeVersion = "0.0.0"
+$runtimePath = $null
+
+foreach ($line in $runtimes) {
+    $collection = ($line | Select-String -Pattern "Microsoft\.WindowsDesktop\.App (.*?) \[(.*?)\]")
+    if ($null -eq $collection -or $collection.Matches.Length -eq 0) {
+        continue
+    }
+
+    $match = $collection.Matches[0]
+    if ($match.Groups[1].Value -lt $runtimeVersion) {
+        continue
+    }
+
+    $runtimeVersion = $match.Groups[1].Value
+    $runtimePath = $match.Groups[2].Value
+}
+
+if ($null -eq $runtimePath) {
+    $message = "Cannot find a suitable Microsoft.WindowsDesktop.App runtime"
+    Write-Host "$("##vso[task.setvariable variable=ErrorMessage]") $message"
+    exit 1
+}
+
+$runtimePath = Join-Path $runtimePath $runtimeVersion
+Write-Host "Found target runtime: $runtimePath"
+$dllPath = Join-Path $runtimePath "PresentationFramework.dll"
+
 if (Test-Path $dllPath -PathType Leaf) {
     Write-Host "Found dll to be copied in: $dllPath"
 } else {
@@ -10,6 +45,7 @@ if (Test-Path $dllPath -PathType Leaf) {
     Write-Host "$("##vso[task.setvariable variable=ErrorMessage]") $message"
     exit 1
 }
+
 Get-ChildItem -Path "$PSScriptRoot/../../tests" -Directory | 
 ForEach-Object { 
     $destination = Join-Path $_.FullName "bin/$buildConfiguration/$targetFramework/"
